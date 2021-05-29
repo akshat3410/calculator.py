3e715977f063b53270410fbf4b322a15f3990c4e
akshat3410 <69171692+akshat3410@users.noreply.github.com>
2021-05-29 15:48:20 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,25 @@
+var1 = int(input("Enter First Number:"))
+var2 = int(input("Enter Second Number:"))
+
+var3 = input("What you want:"+'+, -, *, /:')
+
+if var1 == 56 and var2 == 9 and var3 == '+':
+    print("77")
+elif var3 == '+':
+    print(int(var1) + int(var2))
+
+elif var1 == 93 and var2 == 42 and var3 == '-':
+    print("0.007")
+elif var3 == '-':
+    print(int(var1) - int(var2))
+
+elif var1 == 45 and var2 == 3 and var3 == '*':
+    print("555")
+elif var3 == '*':
+    print(int(var1) * int(var2))
+
+elif var1 == 56 and var2 == 6 and var3 == '/' :
+    print("4")
+
+else:
+    print(int(var1) / int(var2))
